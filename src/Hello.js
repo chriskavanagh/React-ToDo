5bb4f1cd6d596c891d370105d7d6c60e5e412ffb
Chris Kavanagh <ckava3@gmail.com>
2018-10-30 00:44:55 -0400

    initial

---
+++ src/Hello.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+// import { AppRegistry, Text, View } from 'react-native';
+
+class Greeting extends Component {
+    render() {
+        return (
+            <div>Hello {this.props.name}!</div>
+        )
+    }
+}
+
+export default class LotsOfGreetings extends Component {
+    render() {
+        return (
+            <div>
+                <Greeting name="Chris" />
+                <Greeting name="Cam" />
+                <Greeting name="Haley" />
+            </div>
+        )
+    }
+}
+
+  
---
+++ src/todolist.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class ToDoList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userInput: '',
+            list: []
+        }
+    }
+
+    changeUserInput(input) {
+        this.setState({
+            userInput: input
+        });
+    } // end function
+
+    addToList(input) {
+        let listArr = this.state.list;
+        listArr.push(input);
+        this.setState({
+            list: listArr,
+            userInput: ''
+        });
+    } // end function
+
+    render() {
+        return (
+            <div className="to-do-list-main">
+                <input
+                 onChange={ (e)=>this.changeUserInput(e.target.value) }
+                 value={this.state.userInput}
+                 type="text"
+                 />
+                <button onClick={()=>this.addToList(this.state.userInput)}>Press Me</button>
+                <ul>
+                    { this.state.list.map(val => <li>{val}</li>) }
+                </ul>
+            </div>
+        );
+    }
+} // end class
+
+export default ToDoList;
